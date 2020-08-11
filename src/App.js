db7dcc9b33b7cde09b04064416d747af76812cf9
Adit Pinheiro <aditpinheiro@gmail.com>
2020-08-11 14:14:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MyRoutes from './config/routes'
 import { Provider } from 'react-redux';
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faStar, faHeart, faPhone, faEnvelope, faSearch, faUtensils,
   faThumbsUp, faSortAlphaDown, faUserMinus, faDollarSign, faAngleDoubleRight, faPlus,
